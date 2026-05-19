e23e3f24cc71fc31dd6ba60e8273d04e088b902b
Bianca Guimarães <biancagsccabral@gmail.com>
2026-05-18 23:07:54 -0300

    adições de elementos importantes

--- 01 - Estrutura de dados/01 - Listas/18_sorted.py
+++ 01 - Estrutura de dados/01 - Listas/18_sorted.py
@@ -2,3 +2,6 @@ linguagens = ["python", "js", "c", "java", "csharp"]
 
 print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
 print(sorted(linguagens, key=lambda x: len(x), reverse=True))  # ["python", "csharp", "java", "js", "c"]
+
+# nao somos obrigados a passsar tudo isso acima, funciona assim tbm:
+print(sorted(linguagens))
